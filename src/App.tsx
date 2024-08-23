9f76278bbaa1acf056fd7212e9c6051137bce44c
sanchitpasricha <sanchit0229@gmail.com>
2024-08-24 03:26:16 +0530

    Wallet generation functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,42 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect, useState } from "react";
-import { generateMnemonic } from "bip39";
+import { generateMnemonic, mnemonicToSeedSync } from "bip39";
 import { Buffer } from "buffer";
+import { Keypair } from "@solana/web3.js";
+import nacl from "tweetnacl";
+
 import "./App.css";
 
 window.Buffer = Buffer;
 
+interface Key {
+  publicKey: string;
+}
+
 function App() {
   const [mnemonic, setMnemonic] = useState("");
+  const [count, setCount] = useState(0);
+  const [keys, setKeys] = useState<Key[]>([]);
+
+  const path = `m/44'/501'/${count}'/0'`;
 
   const generateRandomMnemonic = () => {
     const mnemonic = generateMnemonic();
     setMnemonic(mnemonic);
     console.log("Generated Mnemonic:", mnemonic);
   };
+
+  const generatewallet = () => {
+    const seed = mnemonicToSeedSync(mnemonic);
+    const derivedSeed = nacl.hash(Buffer.concat([seed, Buffer.from(path)]));
+    const secret = nacl.sign.keyPair.fromSeed(
+      derivedSeed.slice(0, 32)
+    ).secretKey;
+    const publicKey = Keypair.fromSecretKey(secret).publicKey.toBase58();
+    console.log(publicKey);
+    setCount(count + 1);
+    setKeys((prevKeys) => [...prevKeys, { publicKey }]);
+  };
+
   useEffect(() => {
     generateRandomMnemonic();
   }, []);
@@ -29,27 +52,38 @@ function App() {
         <div>
           <input
             type="text"
-            onChange={(e) => {
-              setMnemonic(e.target.value);
-            }}
+            onChange={(e) => setMnemonic(e.target.value)}
             value={mnemonic}
             className="w-4/5 rounded-lg p-2 border text-white font-bold border-gray-800 focus:outline-none bg-gray-700"
           />
           <button
             onClick={() => {
               setMnemonic("");
+              setKeys([]);
+              setCount(0);
             }}
             className="bg-blue-900 p-2 rounded-md text-white font-bold mx-6"
           >
             Clear
           </button>
           <button
-            onClick={() => {}}
-            className="bg-blue-900 p-2 rounded-md text-white font-bold "
+            onClick={generatewallet}
+            className="bg-blue-900 p-2 rounded-md text-white font-bold"
           >
             Generate Wallet
           </button>
         </div>
+        {keys.map((key, index) => (
+          <div
+            key={index}
+            className="bg-gray-700 rounded-lg shadow-lg p-6 mt-8 w-4/5"
+          >
+            <h2 className="text-xl text-white font-bold mb-4">
+              Solana Wallet Public Key {index + 1}
+            </h2>
+            <p className="text-white break-all">{key.publicKey}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
